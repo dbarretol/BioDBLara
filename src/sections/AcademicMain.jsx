16475ffd37a54791420858f07c9e09c7aab51a77
dbarretol <57958754+dbarretol@users.noreply.github.com>
2024-07-23 14:41:38 -0500

    1ra versión de educación

--- src/sections/AcademicMain.jsx
+++ src/sections/AcademicMain.jsx
@@ -5,11 +5,31 @@ function AcademicMain() {
     <>
       <div className="container">
         <h2>Formación académica </h2>
-        <hr></hr>
-        <p>Poner la formación academica aqui</p>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8">
+              <h3>Maestría en Administración de Negocios (MBA)</h3>
+              <h4>ESAN</h4>
+            </div>
+            <div className="col-md-4">
+              <p className="Dates">2023/09 – presente</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <p className="content"><strong>Track de especialización:</strong> Finanzas corporativas.</p>
+            </div>
+          </div>
+        </div>
+        <div className="container">EXP 02</div>
+
       </div>
     </>
   )
 }
 
 export default AcademicMain
+
+{/*        
+        <hr></hr>
+        <p>Poner la formación academica aqui</p>*/}
